03dbeb5cac91f73548c8e263f93d3cb388429e97
AndyLPK247 <AndyLPK247@gmail.com>
2017-12-22 22:11:36 -0500

    added max and min tests for calc functions

--- spec/unit/calculator.function.spec.js
+++ spec/unit/calculator.function.spec.js
@@ -22,17 +22,17 @@ describe("Calculator Functions", function() {
 
     it("should add two positive numbers", function() {
       let value = calc.add(3, 2);
-      expect(value).toEqual(5);
+      expect(value).toBe(5);
     });
   
     it("should add a positive and a negative number", function() {
       let value = calc.add(3, -2);
-      expect(value).toEqual(1);
+      expect(value).toBe(1);
     });
   
     it("should give the same value when adding zero", function() {
       let value = calc.add(3, 0);
-      expect(value).toEqual(3);
+      expect(value).toBe(3);
     });
   
   });
@@ -44,17 +44,17 @@ describe("Calculator Functions", function() {
 
     it("should subtract two numbers", function() {
       let value = calc.subtract(3, 2);
-      expect(value).toEqual(1);
+      expect(value).toBe(1);
     });
   
     it("should subtract two numbers for a negative result", function() {
       let value = calc.subtract(2, 3);
-      expect(value).toEqual(-1);
+      expect(value).toBe(-1);
     });
   
     it("should give the same value when subtracting zero", function() {
       let value = calc.subtract(3, 0);
-      expect(value).toEqual(3);
+      expect(value).toBe(3);
     });
   
   });
@@ -66,27 +66,27 @@ describe("Calculator Functions", function() {
 
     it("should multiply two positive numbers", function() {
       let value = calc.multiply(3, 2);
-      expect(value).toEqual(6);
+      expect(value).toBe(6);
     });
   
     it("should multiply two negative numbers", function() {
       let value = calc.multiply(-3, -2);
-      expect(value).toEqual(6);
+      expect(value).toBe(6);
     });
   
     it("should multiply a positive and a negative", function() {
       let value = calc.multiply(3, -2);
-      expect(value).toEqual(-6);
+      expect(value).toBe(-6);
     });
   
     it("should give the same value when multiplying by one", function() {
       let value = calc.multiply(5, 1);
-      expect(value).toEqual(5);
+      expect(value).toBe(5);
     });
   
     it("should give zero when multiplying by zero", function() {
       let value = calc.multiply(5, 0);
-      expect(value).toEqual(0);
+      expect(value).toBe(0);
     });
   
   });
@@ -98,27 +98,27 @@ describe("Calculator Functions", function() {
 
     it("should divide two positive numbers", function() {
       let value = calc.divide(10, 2);
-      expect(value).toEqual(5);
+      expect(value).toBe(5);
     });
   
     it("should divide two negative numbers", function() {
       let value = calc.divide(-10, -2);
-      expect(value).toEqual(5);
+      expect(value).toBe(5);
     });
   
     it("should divide a positive and a negative", function() {
       let value = calc.divide(-10, 2);
-      expect(value).toEqual(-5);
+      expect(value).toBe(-5);
     });
   
     it("should divide two positive numbers with a decimal result", function() {
       let value = calc.divide(3, 2);
-      expect(value).toEqual(1.5);
+      expect(value).toBe(1.5);
     });
   
     it("should give the same value when dividing by one", function() {
       let value = calc.divide(5, 1);
-      expect(value).toEqual(5);
+      expect(value).toBe(5);
     });
   
     it("should throw an exception when dividing by zero", function() {
@@ -127,5 +127,41 @@ describe("Calculator Functions", function() {
     });
   
   });
+
+
+  // maximum
+
+  describe("maximum", function() {
+
+    [
+      [1, 2, 2],
+      [2, 1, 2],
+      [2, 2, 2],
+    ].forEach(([a, b, expected]) => {
+      it(`should return ${expected} when given ${a} and ${b}`, () => {
+        let value = calc.maximum(a, b);
+        expect(value).toBe(expected);
+      });
+    });
+
+  });
+
   
+  // minimum
+
+  describe("minimum", function() {
+
+    [
+      [1, 2, 1],
+      [2, 1, 1],
+      [1, 1, 1],
+    ].forEach(([a, b, expected]) => {
+      it(`should return ${expected} when given ${a} and ${b}`, () => {
+        let value = calc.minimum(a, b);
+        expect(value).toBe(expected);
+      });
+    });
+
+  });
+
 });
