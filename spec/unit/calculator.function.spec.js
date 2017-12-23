cbd82d8e6f6e16494bfb3f47791c079000c2cff0
AndyLPK247 <AndyLPK247@gmail.com>
2017-12-22 21:43:13 -0500

    split calculator functions and classes into two separate modules

--- spec/unit/calculator.function.spec.js
+++ spec/unit/calculator.function.spec.js
@@ -1,37 +1,60 @@
-const calculator = require('../../lib/calculator');
+// --------------------------------------------------------------------------------
+// Imports
+// --------------------------------------------------------------------------------
+
+const calc = require('../../lib/calculator.functions');
+
+
+// --------------------------------------------------------------------------------
+// Calculator Function Specs
+// 
+// These are very basic tests.
+// Since the calculator functions are stateless, they don't need setup or cleanup.
+// --------------------------------------------------------------------------------
 
 describe("Calculator Functions", function() {
 
-  const NUM_A = 3;
-  const NUM_B = 2;
+  describe("add", function() {
 
-  it("should add two numbers", function() {
-    let value = calculator.add(NUM_A, NUM_B);
-    expect(value).toEqual(5);
+    it("should add two positive numbers", function() {
+      let value = calc.add(3, 2);
+      expect(value).toEqual(5);
+    });
+  
+    it("should add a positive and a negative number", function() {
+      let value = calc.add(3, -2);
+      expect(value).toEqual(1);
+    });
+  
+    it("should give the same value when adding zero", function() {
+      let value = calc.add(3, 0);
+      expect(value).toEqual(3);
+    });
+  
   });
-
+  
   it("should subtract two numbers", function() {
-    let value = calculator.subtract(NUM_A, NUM_B);
+    let value = calc.subtract(3, 2);
     expect(value).toEqual(1);
   });
 
   it("should subtract two numbers for a negative result", function() {
-    let value = calculator.subtract(NUM_B, NUM_A);
+    let value = calc.subtract(2, 3);
     expect(value).toEqual(-1);
   });
 
   it("should multiply two numbers", function() {
-    let value = calculator.multiply(NUM_A, NUM_B);
+    let value = calc.multiply(3, 2);
     expect(value).toEqual(6);
   });
 
   it("should divide two numbers", function() {
-    let value = calculator.divide(NUM_A, NUM_B);
+    let value = calc.divide(3, 2);
     expect(value).toEqual(1.5);
   });
 
   it("should throw an exception when dividing by zero", function() {
-    let divideByZero = function() { calculator.divide(NUM_A, 0); };
+    let divideByZero = function() { calc.divide(3, 0); };
     expect(divideByZero).toThrowError(RangeError, 'Divide-by-zero');
   });
 
